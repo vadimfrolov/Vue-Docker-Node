17860a7e6f94dfe23db5da42e1141d8f957c0fd5
Vadim Frolov <vadimf74@ya.ru>
2024-05-17 19:35:36 +0200

    initial

---
+++ frontend/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <DataDisplay />
+  </div>
+</template>
+
+<script>
+import DataDisplay from './components/DataDisplay.vue';
+
+export default {
+  name: 'App',
+  components: {
+    DataDisplay
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ frontend/src/components/DataDisplay.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+    <h2>{{ hi }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: ''
+    };
+  },
+  mounted() {
+    fetch('http://backend:3000/api/data')
+      .then(response => response.json())
+      .then(data => {
+        this.message = data.message;
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }
+};
+</script>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
